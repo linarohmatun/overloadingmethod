1a858ed308cfa1cd46a5f0555abd225c7b1ba74a
linarohmatun <101708511+linarohmatun@users.noreply.github.com>
2022-04-07 16:47:40 +0200

    Add files via upload

---
+++ numbers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    public class numbers
+    {
+        public int FindMinimum(int number1, int number2)
+        {
+            return number2;
+        }
+        public int FindMinimum(int number1, int number2, float number3)
+        {
+            return number2;
+        }
+        public int FindMaximum(int number1, int number2)
+        {
+            return number1;
+        }
+        public int FindMaximum(int number1, int number2, float number3)
+        {
+            return (int)4;    
+        }
+
+    }
+}
